f1a9f2c37cb0b93a779c37be6cd8b95c3709d58f
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2017-11-02 07:06:13 +0000

    test

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php header( 'Location: /Construction.html') ;  ?>
+<?php
+ 
+require 'vendor/autoload.php';
+ 
+use PostgreSQLTutorial\Connection as Connection;
+ 
+try {
+    Connection::get()->connect();
+    echo 'A connection to the PostgreSQL database sever has been established successfully.';
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
+
+?>
