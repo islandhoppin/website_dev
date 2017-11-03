b77f016030dc33f131122334fa283c3b375b373d
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2017-11-03 05:12:38 +0000

    test

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@ $password = $dbopts["pass"];
 $host = $dbopts["host"];
 $port = $dbopts["port"];
 $dbname = ltrim($dbopts["path"],'/');
-$conn = ("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
+$conn = ("pgsql:host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 try{
 	// create a PostgreSQL database connection
 	echo $conn;
@@ -15,7 +15,7 @@ try{
  
 	// display a message if connected to the PostgreSQL successfully
 	if($dbconn){
-		echo "Connected to the <strong>$db</strong> database successfully!";
+		echo "Connected to the <strong>$dbname</strong> database successfully!";
 	}
 }catch (PDOException $e){
 	// report error message
