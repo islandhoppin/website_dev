68f05a570fd7969a89a9d82d01bb1769a803d217
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2017-11-03 04:48:19 +0000

    minor changes

--- index2.php
+++ index2.php
@@ -1,14 +1,24 @@
 <?php
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$driver = 'pgsql';
+$user = $dbopts["user"];
+$password = $dbopts["user"];
+$host = $dbopts["host"];
+$port = $dbopts["port"];
+$dbname = $dbopts["port"];
+
+try{
+	// create a PostgreSQL database connection
+	$dbconn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
  
-require 'vendor/autoload.php';
- 
-use IslandHoppinPostgreSQL\Connection as Connection;
- 
-try {
-    Connection::get()->connect();
-    echo 'A connection to the PostgreSQL database sever has been established successfully.';
-} catch (\PDOException $e) {
-    echo $e->getMessage();
+	// display a message if connected to the PostgreSQL successfully
+	if($dbconn){
+		echo "Connected to the <strong>$db</strong> database successfully!";
+	}
+}catch (PDOException $e){
+	// report error message
+	echo $e->getMessage();
 }
+
 
 ?>
