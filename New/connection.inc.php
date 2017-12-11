d758db586857a70af93a333cd7d118833adf9a44
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2017-12-11 06:22:48 +0000

    add FAQ

--- New/connection.inc.php
+++ New/connection.inc.php
@@ -1,6 +1,5 @@
 <?php
-//$dbopts = parse_url(getenv('DATABASE_URL'));
-$dbopts = parse_url('postgres://jwfpbjsxcbuyxn:b1e52211e1e054a63b770b1a0dd63095846ae5024d6ea63f213bf6a505fc93ea@ec2-23-23-228-115.compute-1.amazonaws.com:5432/d55rf3q3mqghto');
+$dbopts = parse_url(getenv('DATABASE_URL'));
 $driver = 'pgsql';
 $user = $dbopts["user"];
 $password = $dbopts["pass"];
