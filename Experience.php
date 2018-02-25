e6f2e29cd63247cf3e7f8dd48b954ae811e70100
Brandon Mikelaitis <islandhoppincharters@gmail.com>
2018-02-25 02:24:47 +0000

    Food, pic, vids

--- Experience.php
+++ Experience.php
@@ -4,7 +4,7 @@
 	$sqltut = $dbconn->prepare("Select news_id, header, update, image, blank_1 From customertut ORDER BY news_id DESC") ; 
 	$sqlfood = $dbconn->prepare("Select food_title, show, image, blank_1 From food WHERE show = 'Yes' ORDER BY blank_1 DESC") ; 
 	$sqlcustpics = $dbconn->prepare("Select pic_title, show, image, blank_1 From customerpic WHERE show = 'Yes' ORDER BY blank_1 DESC") ; 
-	$sqlcustvids = $dbconn->prepare("Select vid_title, show, vid_link, image, blank_1 From customervid WHERE show = 'Yes' ORDER BY blank_1 DESC") ; 
+	$sqlcustvids = $dbconn->prepare("Select vid_title, show, video_link, image, blank_1 From customervid WHERE show = 'Yes' ORDER BY blank_1 DESC") ; 
       // Execute the query, if there were variables, they could be bound within the brackets
     $sqltut->execute() ;
     $sqlfood->execute() ;
